3db8780d5199a02c16728d645ba368c942eaf4db
Async <me@astrohweston.xyz>
2026-07-01 12:16:24 +0200

    feat(components): add Spring type and SpringCard component

--- app/components/SpringCard.vue
+++ app/components/SpringCard.vue
@@ -9,3 +9,28 @@ const temperatureLabel = computed(() => {
   return `${props.spring.temperature.min}–${props.spring.temperature.max}°F`;
 });
 </script>
+
+<template>
+  <NuxtLink :to="`/springs/${spring.id}`">
+    <div>
+      <div>
+        <h3>
+          {{ spring.name }}
+        </h3>
+        <span>
+          {{ spring.type }}
+        </span>
+      </div>
+ 
+      <p>
+        {{ spring.description.slice(0, 120) }}...
+      </p>
+ 
+      <div>
+        <span>{{ spring.location.region }}, {{ spring.location.country }}</span>
+        <span>{{ temperatureLabel }}</span>
+        <span>{{ spring.elevation.toLocaleString() }} ft</span>
+      </div>
+    </div>
+  </NuxtLink>
+</template>
